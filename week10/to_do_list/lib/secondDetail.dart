c33998e57406ee83d98b724e05ed5e0e55f9afd6
petcu1004 <msw4585@gmail.com>
2022-01-22 00:59:56 +0900

    할 일을 추가하는 기능 만들기

--- week10/to_do_list/lib/secondDetail.dart
+++ week10/to_do_list/lib/secondDetail.dart
@@ -4,18 +4,24 @@ import 'package:flutter/material.dart';
 class SecondDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = new TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Second Detail'),
       ),
       body: Container(
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/third'); // /second로 뒤돌아 갈 수 없음.
-            },
-            child: Text('세 번째 페이지로 이동하기'),
-          ),
+          child: Column(children: <Widget>[
+            TextField(
+              controller: controller,
+              keyboardType: TextInputType.text,
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).pop(controller.value.text);
+            }, child: Text('Todo 저장하기'),
+            ),
+          ],)
         ),
       ),
     );
