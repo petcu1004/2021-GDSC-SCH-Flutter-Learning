0d2c25d2918f90645362881ee8e3d60aad6b2604
petcu1004 <msw4585@gmail.com>
2022-01-21 16:50:30 +0900

    GridView

--- week8/list_gird_view_buildcontext/lib/grid.dart
+++ week8/list_gird_view_buildcontext/lib/grid.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Grids extends StatefulWidget {
-  const Grids({ Key? key }) : super(key: key);
+  const Grids({Key? key}) : super(key: key);
 
   @override
   _GridsState createState() => _GridsState();
 }
 
 class _GridsState extends State<Grids> {
+  List<String> s = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, //한 행의 열의 갯수
+              mainAxisSpacing: 4.0, //행의 구분 너비
+              crossAxisSpacing: 4.0, //열의 구분 너비
+              childAspectRatio: 2 / 3),
+          itemCount: s.length,
+          itemBuilder: (ctx, index) {
+            return Container(
+              color: Colors.green[100],
+              child: Center(
+                child: Text('${s[index]}'),
+              ),
+            );
+          }),
     );
   }
 }
