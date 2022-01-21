5b79fb3686ae7ca89325b3b9919ec1b41556ee2e
petcu1004 <msw4585@gmail.com>
2022-01-21 16:58:45 +0900

    map 형식 사용

--- week8/list_gird_view_buildcontext/lib/grid.dart
+++ week8/list_gird_view_buildcontext/lib/grid.dart
@@ -8,7 +8,15 @@ class Grids extends StatefulWidget {
 }
 
 class _GridsState extends State<Grids> {
-  List<String> s = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  // List<String> s = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  List<Map<String, dynamic>> m = [
+    {"num": "1", "eng": "one"},
+    {"num": "2", "eng": "two"},
+    {"num": "3", "eng": "three"},
+    {"num": "4", "eng": "four"},
+    {"num": "5", "eng": "five"},
+    {"num": "6", "eng": "six"}
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +26,20 @@ class _GridsState extends State<Grids> {
               crossAxisCount: 3, //한 행의 열의 갯수
               mainAxisSpacing: 4.0, //행의 구분 너비
               crossAxisSpacing: 4.0, //열의 구분 너비
-              childAspectRatio: 2 / 3),
-          itemCount: s.length,
+              childAspectRatio: 1.0),
+          itemCount: m.length,
           itemBuilder: (ctx, index) {
             return Container(
               color: Colors.green[100],
               child: Center(
-                child: Text('${s[index]}'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('${m[index]['num']} 영어로'),
+                    SizedBox(height: 10),
+                    Text('${m[index]['eng']}')
+                  ],
+                ),
               ),
             );
           }),
